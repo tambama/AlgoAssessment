5e06225cfdbb0ddf0c8c24836d46e764ed5af100
peniel tambama <tambama.peniel@outlook.com>
2021-03-15 00:33:09 +0200

    Format Number Solution

--- TGS.Challenge/FormatNumber.cs
+++ TGS.Challenge/FormatNumber.cs
@@ -4,7 +4,7 @@ namespace TGS.Challenge
 {
     /*
         Devise a function that takes an input 'n' (integer) and returns a string that is the
-        decimal representation of that number grouped by commas after every 3 digits. 
+        decimal representation of that value grouped by commas after every 3 digits. 
         
         NOTES: You cannot use any built-in formatting functions to complete this task.
 
@@ -26,7 +26,33 @@ namespace TGS.Challenge
     {
         public string Format(int value)
         {
-            return string.Empty;
+            // any value with less than 4 digits will not have commas 
+            if ((string.Empty + value).Length < 4) return string.Empty + value;
+
+            // initialize formatted string
+            var formattedNumber = string.Empty;
+
+            // convert the value string to an array of digits 
+            var valueArray = (string.Empty + value).ToCharArray();
+
+            var arrayLength = valueArray.Length;
+
+            // a comma is added to the right of a digit if the modulus of the
+            // array length minus the index of the digit is 1
+            // and if the value of digits to the right of the digit is greater than 3
+            for (var i = 0; i < arrayLength; i++)
+            {
+                if ((arrayLength - i) % 3 == 1 && arrayLength - i > 3)
+                {
+                    formattedNumber += valueArray[i] + ",";
+                }
+                else
+                {
+                    formattedNumber += valueArray[i];
+                }
+            }
+
+            return formattedNumber;
         }
     }
 }
